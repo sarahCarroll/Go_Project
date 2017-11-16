15bcf8d51cd8eda3edc4f89ae927d59e0387a30a
sarahc123 <g00330821@gmit.ie>
2017-11-16 11:53:32 +0000

    diffuculty making responces with array x

--- index.go
+++ index.go
@@ -9,8 +9,10 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
+	"math/rand"
 	"net/http"
+	"regexp"
+	"time"
 )
 
 var chatter string
@@ -20,33 +22,62 @@ type Data struct {
 	//Chat    []string
 }
 
+var responses = []string{
+	"I’m not sure what you’re trying to say. Could you explain it to me?",
+	"How does that make you feel?",
+	"Why do you say that?",
+}
+
+func ElizaResponse(input string) string {
+
+	if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, input); matched {
+		//match string
+
+		return "why dont you tell me more about your father?"
+
+	}
+
+	return responses[rand.Intn(len(responses))]
+
+}
+
 //The main function begins with a call to http.HandleFunc, which tells the http package to handle all requests to the web root ("/") with handler.
 func templateHandler(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm() //needed to parse message to print out in console
-	var z string
+	//var z string
 	x := r.Form["usermsg"]
 
-	m := Data{Message: "Eliza robot chat", Chat: "" + z}
+	m := Data{Message: "Eliza robot chat", Chat: ""}
 
 	t, _ := template.ParseFiles("guess.html")
 
-	x, err := r.URL.Query()["usermsg"]
+	/*x, err := r.URL.Query()["usermsg"]
 	// if not found execute the template and exit
 	if !err || len(x) < 1 {
 		log.Println("Empty Input!!")
 		// execute the template with the message
 		t.Execute(w, m)
 		return
-	} // if
-	z = "" + x[0]
+	} // if*/
+	//z = "" + x[0]
 	//r.Form["chatbox"] = x
 
 	//fmt.Fprintf(w, "Hello, %s!", r.Form["usermsg"])
-	fmt.Println("User Input", x)
+
+	if len(x) > 0 {
+		rand.Seed(time.Now().UTC().UnixNano())
+		fmt.Println("User Input", x)
+		//fmt.Println(x)
+		//difficulty tryint to get array to be matched with elizas "father"
+		fmt.Println(ElizaResponse(""))
+		fmt.Println()
+	}
 	//fmt.fprintf(w, &x)
 
-	//t.Execute(w, &m)
+	t.Execute(w, &m)
+
+	//http.HandleFunc("/", inputHandler)
 
 }
 
@@ -61,13 +92,13 @@ func inputHandler(w http.ResponseWriter, r *http.Request) {
 
 	t, _ := template.ParseFiles("guess.html")
 
-	//t.Execute(w, m)
+	t.Execute(w, m)
 
 	//r.Form["chatbox"] = x
 
 	//fmt.Fprintf(w, "Hello, %s!", r.Form["usermsg"])
 	fmt.Println("User Input", x)
-	fmt.fprintf(w, &x)
+	//fmt.fprintf(w, &x)
 
 	//t.Execute(w, &m)
 
@@ -76,8 +107,8 @@ func inputHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	//call handler function
-	http.HandleFunc("/guess", templateHandler)
-	http.HandleFunc("/", inputHandler)
+	http.HandleFunc("/", templateHandler)
+	//http.HandleFunc("/guess", inputHandler)
 
 	//http.HandleFunc("/guess", templateHandler)
 	http.ListenAndServe(":8080", nil)
