2ec99a6dd588550e0d70fa5b2d8f439eab0b8559
sarahc123 <g00330821@gmit.ie>
2017-11-16 18:36:22 +0000

    break throw

--- index.go
+++ index.go
@@ -8,24 +8,24 @@ package main
 //To use the net/http package, it must be imported
 import (
 	"fmt"
-	"html/template"
 	"math/rand"
-	"net/http"
 	"regexp"
 	"time"
+	//	"html"
+	"html/template"
+	"net/http"
 )
-
-var chatter string
-
-type Data struct {
-	Message, Chat string
-	//Chat    []string
-}
 
 var responses = []string{
 	"I’m not sure what you’re trying to say. Could you explain it to me?",
 	"How does that make you feel?",
 	"Why do you say that?",
+}
+
+var chatter string
+
+type Data struct {
+	Message, Chat, Flag string
 }
 
 func ElizaResponse(input string) string {
@@ -37,70 +37,60 @@ func ElizaResponse(input string) string {
 
 	}
 
+	re := regexp.MustCompile(`(?i)I am ([^.?!]*)[.?!]?`)
+	if matched := re.MatchString(input); matched {
+		return re.ReplaceAllString(input, "How do you know you are $1?")
+	}
+
 	return responses[rand.Intn(len(responses))]
 
 }
 
 //The main function begins with a call to http.HandleFunc, which tells the http package to handle all requests to the web root ("/") with handler.
+
 func templateHandler(w http.ResponseWriter, r *http.Request) {
 
+	var z, name, flagit, resp string
+
 	r.ParseForm() //needed to parse message to print out in console
-	//var z string
 	x := r.Form["usermsg"]
-
-	m := Data{Message: "Eliza robot chat", Chat: ""}
-
-	t, _ := template.ParseFiles("guess.html")
-
-	/*x, err := r.URL.Query()["usermsg"]
-	// if not found execute the template and exit
-	if !err || len(x) < 1 {
-		log.Println("Empty Input!!")
-		// execute the template with the message
-		t.Execute(w, m)
-		return
-	} // if*/
-	//z = "" + x[0]
-	//r.Form["chatbox"] = x
-
-	//fmt.Fprintf(w, "Hello, %s!", r.Form["usermsg"])
+	flag := r.Form["flag"]
 
 	if len(x) > 0 {
-		rand.Seed(time.Now().UTC().UnixNano())
 		fmt.Println("User Input", x)
-		//fmt.Println(x)
-		//difficulty tryint to get array to be matched with elizas "father"
-		fmt.Println(ElizaResponse(""))
-		fmt.Println()
+
+		// build logic here to interpret question and set z appropriately
+		// flag = 1 on initial query
+		if flag[0] == "1" {
+			name = x[0]
+			z = chatter + "Hello " + name + "! What is your query?"
+			flagit = "2"
+		}
+
+		if flag[0] == "2" {
+			rand.Seed(time.Now().UTC().UnixNano())
+			//fmt.Println("User Input:", x[0])
+			//fmt.Println(x)
+			//difficulty tryint to get array to be matched with elizas "father"
+
+			resp = ElizaResponse(x[0])
+			fmt.Println(resp)
+			z = chatter + "Eliza:" + resp
+			fmt.Println()
+			flagit = "2"
+		}
+	} else {
+		z = "Welcome, what is your name?      "
+		flagit = "1"
 	}
-	//fmt.fprintf(w, &x)
+
+	chatter = z // save conversation to date
+
+	m := Data{Message: "Eliza Robot Chat", Chat: "   " + chatter, Flag: "" + flagit}
+
+	t, _ := template.ParseFiles("dadguess.html")
 
 	t.Execute(w, &m)
-
-	//http.HandleFunc("/", inputHandler)
-
-}
-
-func inputHandler(w http.ResponseWriter, r *http.Request) {
-
-	r.ParseForm() //needed to parse message to print out in console
-	var z string
-	x := r.Form["usermsg"]
-	z = "" + x[0]
-
-	m := Data{Message: "Eliza robot chat", Chat: "" + z}
-
-	t, _ := template.ParseFiles("guess.html")
-
-	t.Execute(w, m)
-
-	//r.Form["chatbox"] = x
-
-	//fmt.Fprintf(w, "Hello, %s!", r.Form["usermsg"])
-	fmt.Println("User Input", x)
-	//fmt.fprintf(w, &x)
-
-	//t.Execute(w, &m)
 
 }
 
@@ -108,8 +98,5 @@ func main() {
 
 	//call handler function
 	http.HandleFunc("/", templateHandler)
-	//http.HandleFunc("/guess", inputHandler)
-
-	//http.HandleFunc("/guess", templateHandler)
 	http.ListenAndServe(":8080", nil)
 }
