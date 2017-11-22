48d936512e03346b07acadb630f58853a1e3c68f
sarahc123 <g00330821@gmit.ie>
2017-11-22 12:23:00 +0000

    attempt with ajax

--- index.go
+++ index.go
@@ -15,39 +15,10 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"math/rand"
 	"net/http"
 	"regexp"
 	"strings"
 )
-
-func Reflect(input string) string {
-	// Split the input on word boundaries.
-	boundaries := regexp.MustCompile(`\b`)
-	tokens := boundaries.Split(input, -1)
-
-	// List the reflections.
-	reflections := [][]string{
-		{`I`, `you`},
-		{`me`, `you`},
-		{`you`, `me`},
-		{`my`, `your`},
-		{`your`, `my`},
-	}
-
-	// Loop through each token, reflecting it if there's a match.
-	for i, token := range tokens {
-		for _, reflection := range reflections {
-			if matched, _ := regexp.MatchString(reflection[0], token); matched {
-				tokens[i] = reflection[1]
-				break
-			}
-		}
-	}
-
-	// Put the tokens back together.
-	return strings.Join(tokens, ``)
-}
 
 var responses = []string{
 	"I’m not sure what you’re trying to say. Could you explain it to me?",
@@ -75,8 +46,35 @@ func ElizaResponse(input string) string {
 	if matched := re.MatchString(input); matched {
 		return re.ReplaceAllString(input, "How do you know you are $1?")
 	}
+
+	boundaries := regexp.MustCompile(`\b`)
+	tokens := boundaries.Split(input, -1)
+
+	// List the reflections.
+	reflections := [][]string{
+		{`I`, `you`},
+		{`me`, `you`},
+		{`you`, `me`},
+		{`my`, `your`},
+		{`your`, `my`},
+	}
+
+	// Loop through each token, reflecting it if there's a match.
+	for i, token := range tokens {
+		for _, reflection := range reflections {
+			if matched, _ := regexp.MatchString(reflection[0], token); matched {
+				tokens[i] = reflection[1]
+				break
+			}
+		}
+	}
+
+	// Put the tokens back together.
+
 	//return the response
-	return responses[rand.Intn(len(responses))]
+	//return responses[rand.Intn(len(responses))]
+
+	return strings.Join(tokens, ``)
 
 }
 
@@ -84,7 +82,7 @@ func ElizaResponse(input string) string {
 
 func templateHandler(w http.ResponseWriter, r *http.Request) {
 
-	var z, flagit, resp, ans string
+	var z, flagit, resp string
 
 	r.ParseForm() //needed to parse message to print out in console
 	x := r.Form["usermsg"]
@@ -108,10 +106,8 @@ func templateHandler(w http.ResponseWriter, r *http.Request) {
 			//	flag = 2 on subsequent queries
 			if len(x[0]) > 0 {
 				resp = ElizaResponse(x[0])
-				ans = Reflect(x[0])
-				fmt.Println(Reflect("You are my friend."))
+				//fmt.Println(Reflect("You are my friend."))
 				z = name + ": " + x[0] + "\nEliza: " + resp + "\n\n"
-				z = name + ": " + x[0] + "\nEliza: " + ans + "\n\n"
 			}
 
 			// z + = ElizaResponse(x[0]) + "\n\n"
@@ -143,5 +139,9 @@ func main() {
 
 	//call handler function
 	http.HandleFunc("/", templateHandler)
+
+	fs := http.FileServer(http.Dir(""))
+	http.Handle("/user-input", fs)
+
 	http.ListenAndServe(":8080", nil)
 }
